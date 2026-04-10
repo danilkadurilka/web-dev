162baff64a1f8217a3ed77b75244630bfdf2d337
Dany <danilka.kurs2007@gmail.com>
2026-04-10 17:20:45 +0700

    Лабораторная работа 4. Выполнены все задания, привязаны события к кнопкам

--- lab4/lab4_script.js
+++ lab4/lab4_script.js
@@ -20,9 +20,7 @@ function task3()
     for (let i = 2; i <= 10; i++)
     {
         if (i % 2 === 0)
-        {
             alert(`Выведено число ${i}`);
-        }
     }
 }
 
@@ -58,13 +56,47 @@ function task5()
             break;
         }
         else
-        {
             alert(`Введено число ${number}. Оно не больше 100. Повторите ввод!`);
-        }
     }
+}
+
+function isPrimeTask6(num)
+{
+    let primes = []
+    if (num<2)
+        return false;
+    for (let i = 2; i <= num; i++)
+    {
+        let count_divider = 0;
+        for (let j = 1; j <= i; j++)
+        {
+            if (i % j === 0)
+                count_divider++;
+        }
+        if (count_divider === 2)
+            primes.push(i);
+    }
+    alert(`${primes}`);
+}
+
+function task6()
+{
+    let numInput = document.getElementById("primeN");
+    let n = parseInt(numInput.value, 10);
+    if (isNaN(n) || n<2)
+    {
+        alert(`Ошибка! Введите число больше 2. Повторите ввод!`);
+        return;
+    }
+    isPrimeTask6(n);
 }
 
 document.addEventListener("DOMContentLoaded", () =>
 {
     document.getElementById("buttonTask1").addEventListener("click", task1);
+    document.getElementById("buttonTask2").addEventListener("click", task2);
+    document.getElementById("buttonTask3").addEventListener("click", task3);
+    document.getElementById("buttonTask4").addEventListener("click", task4);
+    document.getElementById("buttonTask5").addEventListener("click", task5);
+    document.getElementById("buttonTask6").addEventListener("click", task6);
 });
